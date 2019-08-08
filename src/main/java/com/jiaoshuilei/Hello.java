18c041841290125f93410309bedbaa919c954070
jiaoshuilei <544659122@qq.com>
2019-08-08 09:30:12 +0800

    改变con

---
+++ src/main/java/com/jiaoshuilei/Hello.java
@@ -0,0 +1,27 @@
+/**   
+ * Copyright © 2019 公司名. All rights reserved.
+ * 
+ * @Title: Hello.java 
+ * @Prject: Rikao_05
+ * @Package: com.jiaoshuilei 
+ * @Description: TODO
+ * @author: 焦大司令   
+ * @date: 2019年8月8日 上午8:13:01 
+ * @version: V1.0   
+ */
+package com.jiaoshuilei;
+
+/** 
+ * @ClassName: Hello 
+ * @Description: TODO
+ * @author: 焦大司令
+ * @date: 2019年8月8日 上午8:13:01  
+ */
+public class Hello {
+	public static void main(String[] args) {
+		
+		System.out.println("helloWord");
+		System.out.println("helloWord9");
+	}
+
+}
---
+++ src/main/java/com/jiaoshuilei/HelloController.java
@@ -0,0 +1,23 @@
+/**   
+ * Copyright © 2019 公司名. All rights reserved.
+ * 
+ * @Title: HelloController.java 
+ * @Prject: Rikao_05
+ * @Package: com.jiaoshuilei 
+ * @Description: TODO
+ * @author: 焦大司令   
+ * @date: 2019年8月8日 上午9:09:43 
+ * @version: V1.0   
+ */
+package com.jiaoshuilei;
+
+/** 
+ * @ClassName: HelloController 
+ * @Description: TODO
+ * @author: 焦大司令
+ * @date: 2019年8月8日 上午9:09:43  
+ */
+public class HelloController {
+
+	//1111
+}
